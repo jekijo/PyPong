90a8709a960f9e298f11b7087237b9608ce6085e
jekijo <123jkjones@gmail.com>
2022-03-10 13:08:14 -0600

    paddle movement

--- solution.py
+++ solution.py
@@ -14,9 +14,10 @@ WHITE = (255, 255, 255)
 
 PADDLE_WIDTH, PADDLE_HEIGHT = 20, 100
 
-
+# paddle creation
 class Paddle:
     COLOR = WHITE
+    VEL = 4
 
     def __init__(self, x, y, width, height):
         self.x = x
@@ -27,6 +28,12 @@ class Paddle:
     def draw(self, win):
         pygame.draw.rect(win, self.COLOR, (self.x, self.y, self.width, self.height))
 
+    def move(self, up=True):
+        if up:
+            self.y -= self.VEL
+        else:
+            self.y += self.VEL
+
 
 def draw(win, paddles):
     win.fill(BLACK)
@@ -35,13 +42,27 @@ def draw(win, paddles):
 
     pygame.display.update()
 
+
+def handle_paddle_movement(keys, left_paddle, right_paddle):
+    if keys[pygame.K_w] and left_paddle.y - left_paddle.VEL >= 0:
+        left_paddle.move(up=True)
+    if keys[pygame.K_s] and left_paddle.y + left_paddle.VEL + left_paddle.height <= HEIGHT:
+        left_paddle.move(up=False)
+
+    if keys[pygame.K_UP] and right_paddle.y - right_paddle.VEL >= 0:
+        right_paddle.move(up=True)
+    if keys[pygame.K_DOWN] and right_paddle.y + right_paddle.VEL + right_paddle.height <= HEIGHT:
+        right_paddle.move(up=False)
+    
+
+
 def main():
     run = True
     clock = pygame.time.Clock()
 
     left_paddle = Paddle(10, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
     right_paddle = Paddle(WIDTH - 10 - PADDLE_WIDTH, HEIGHT//2 - PADDLE_HEIGHT//2, PADDLE_WIDTH, PADDLE_HEIGHT)
-    
+
     while run:
         clock.tick(FPS) # setting frame rate
         draw(WIN, [left_paddle, right_paddle])
@@ -49,6 +70,9 @@ def main():
             if event.type == pygame.QUIT:
                 run = False
                 break
+
+        keys = pygame.key.get_pressed()
+        handle_paddle_movement(keys, left_paddle, right_paddle)
 
     pygame.quit()
 
